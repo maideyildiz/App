b829df202b84322d0531dbb80113bea7ac309759
Maide Yıldız <maideyildizz@gmail.com>
2021-12-03 17:37:17 +0300

    pulled data :)

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import * as firebase from "firebase";
+import "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRexWIq-qD0Gwjg9JcLmGrvdUVmaSB8pI",
-  authDomain: "reactnativeapp-fd86c.firebaseapp.com",
-  projectId: "reactnativeapp-fd86c",
-  storageBucket: "reactnativeapp-fd86c.appspot.com",
-  messagingSenderId: "840059609235",
-  appId: "1:840059609235:web:35b343b4e08acfc1bd6d66",
+  apiKey: "AIzaSyCgVJRwDN1elyNir1zCFq8WPA0hkfpPcuE",
+  authDomain: "mobileapp-94d3d.firebaseapp.com",
+  projectId: "mobileapp-94d3d",
+  storageBucket: "mobileapp-94d3d.appspot.com",
+  messagingSenderId: "406914191149",
+  appId: "1:406914191149:web:f028b63f44fe3c8ace8da4"
 };
 
 let app;
@@ -20,6 +21,7 @@ if (firebase.apps.length === 0) {
   app = firebase.app();
 }
 
+const fireDB = app.firestore();
 const auth = firebase.auth();
 
-export { auth };
+export { auth,fireDB };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import { useState,useEffect } from "react/cjs/react.development";
+import { fireDB } from "../../firebase";
 
 function Home() {
-  return (
+  const [recipes, setRecipes] = useState();
+
+    useEffect(() => {
+      fetchMyRecipes();
+    }, []);
+  
+    const fetchMyRecipes = async () => {
+      const response = await fireDB.collection('Tarifler').
+      doc('6D1ZpMGBvcXfT3AXyBWL').get();
+      setRecipes(response._document.data.internalValue.root.value.internalValue);
+      console.log(response._document.data.internalValue.root.value.internalValue);
+    };
+
+
+  if (!recipes) 
+    return (
     <View>
-      <Text>ANASAYFA</Text>
+        <Text>Loading...</Text>
+        </View>
+    )
+  return (
+    
+    <View>
+      <Text>{recipes}</Text>
     </View>
-  );
+  )
 }
 
 export default Home;
