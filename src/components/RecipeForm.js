1b1f539a323df36c6aae64dfe2d37f4ee7ee2124
Maide Yıldız <maideyildizz@gmail.com>
2021-12-18 22:27:24 +0300

    added new recipe

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,44 +1,55 @@
 //Push Notification unutma
 import React from "react";
-import { SafeAreaView, StyleSheet, TextInput, Text ,Button} from "react-native";
+import { useState } from "react/cjs/react.development";
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Text,
+  Button,
+} from "react-native";
+import { fireDB } from "../../firebase";
 
 function RecipeForm() {
-  const [text, onChangeText] = React.useState();
-  const [number, onChangeNumber] = React.useState();
-
+  const [Name, changeName] = useState();
+  const [Materials, changeMaterials] = useState();
+  const [Time, changeTime] = useState();
+  const [Person, changePerson] = useState();
+  const [Preparation, changePreparation] = useState();
+  const addRecipe = async () => {
+    fireDB.collection("Tarifler").add({
+      Name,
+      Materials,
+      Time,
+      Person,
+      Preparation,
+    });
+  };
   return (
     <SafeAreaView>
       <Text>Tarif İsmi</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
+      <TextInput style={styles.input} onChangeText={(val) => changeName(val)} />
       <Text>Malzemeler</Text>
       <TextInput
+        multiline
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={(val) => changeMaterials(val)}
       />
       <Text>Kaç kişilik?</Text>
       <TextInput
+        keyboardType="numeric"
         style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
+        onChangeText={(val) => changePerson(val)}
       />
       <Text>Hazırlanma süresi</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
+      <TextInput style={styles.input} onChangeText={(val) => changeTime(val)} />
       <Text>Hazırlanışı</Text>
       <TextInput
+        multiline
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={(val) => changePreparation(val)}
       />
-      <Button buttonText="Tarifi Kaydet" ></Button>
+      <Button title="Tarifi Kaydet" onPress={addRecipe} />
     </SafeAreaView>
   );
 }
