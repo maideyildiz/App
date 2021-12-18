258fcc7ae6ccffa65fcb355443a4e7eda58475ea
Maide Yıldız <maideyildizz@gmail.com>
2021-12-18 17:44:47 +0300

    changes

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,12 +1,53 @@
+//Push Notification unutma
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, Text ,Button} from "react-native";
 
 function RecipeForm() {
+  const [text, onChangeText] = React.useState();
+  const [number, onChangeNumber] = React.useState();
+
   return (
-    <View>
-      <Text>recipe form</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Tarif İsmi</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text>Malzemeler</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text>Kaç kişilik?</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+      />
+      <Text>Hazırlanma süresi</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text>Hazırlanışı</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Button buttonText="Tarifi Kaydet" ></Button>
+    </SafeAreaView>
   );
 }
-
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 export default RecipeForm;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,68 +1,65 @@
 import React from "react";
-import {
-  ScrollView,
-  View,
-  Text,
-  FlatList,
-  Image
-} from 'react-native';
-import { useState,useEffect } from "react/cjs/react.development";
+import { ScrollView, View, Text, FlatList, Image } from "react-native";
+import { useState, useEffect } from "react/cjs/react.development";
 import { fireDB } from "../../firebase";
 
 function RecipeList() {
-    const [recipes, setRecipes] = useState();
+  const [recipes, setRecipes] = useState();
 
-    useEffect(() => {
-      fetchMyRecipes();
-    }, []);
-  
-    const fetchMyRecipes = async () => {
-      const response = await fireDB.collection('Tarifler');
-      const unsubscribe = response.onSnapshot(getRecipeData);
-    };
-    const getRecipeData = (querySnapshot) => {
-      const recs = [];
-      querySnapshot.forEach((res) => {
-        const { Materials,Name,Person,Preparation,Time } = res.data();
-        recs.push({
-          Materials,
-          Name,
-          Person,
-          Preparation,
-          Time
-        });
+  useEffect(() => {
+    fetchMyRecipes();
+  }, []);
+
+  const fetchMyRecipes = async () => {
+    const response = await fireDB.collection("Tarifler");
+    const unsubscribe = response.onSnapshot(getRecipeData);
+  };
+  const getRecipeData = (querySnapshot) => {
+    const recs = [];
+    querySnapshot.forEach((res) => {
+      const { Materials, Name, Person, Preparation, Time } = res.data();
+      recs.push({
+        Materials,
+        Name,
+        Person,
+        Preparation,
+        Time,
       });
-      setRecipes(recs)
-    }
+    });
+    setRecipes(recs);
+  };
 
-    if (!recipes) 
+  if (!recipes)
     return (
-    <View>
+      <View>
         <Text>Loading...</Text>
-        </View>
-    )
-    console.log(recipes)
+      </View>
+    );
+  console.log(recipes);
   return (
     <ScrollView>
-      {recipes.map((recipe, index) => 
-      <Text key={index}>
-        
-      <Image source={{uri:"https://img.icons8.com/external-becris-lineal-color-becris/64/000000/external-dish-ketogenic-diet-becris-lineal-color-becris.png"}} alt="recipe" 
-      resizeMode='cover'
-      style={{
-        width:100,
-        height:100,
-      }}/>
-      <Text h1>{recipe.Name}</Text>
-        <Text>Hazırlanma süresi: {recipe.Time}</Text>
-        <Text>Kaç kişilik?: {recipe.Person}</Text>
-        <Text>Malzemeler: {recipe.Materials}</Text>
-        <Text>Hazırlanışı: {recipe.Preparation}</Text>
-    </Text>)}
+      {recipes.map((recipe, index) => (
+        <Text key={index}>
+          <Image
+            source={{
+              uri: "https://img.icons8.com/external-becris-lineal-color-becris/64/000000/external-dish-ketogenic-diet-becris-lineal-color-becris.png",
+            }}
+            alt="recipe"
+            resizeMode="cover"
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+          <Text h1>{recipe.Name}</Text>
+          <Text>Hazırlanma süresi: {recipe.Time}</Text>
+          <Text>Kaç kişilik?: {recipe.Person}</Text>
+          <Text>Malzemeler: {recipe.Materials}</Text>
+          <Text>Hazırlanışı: {recipe.Preparation}</Text>
+        </Text>
+      ))}
     </ScrollView>
   );
 }
 
 export default RecipeList;
-
-
